a9f3ddf7f8b8527b7e1f926143610dc125cca1e3
MatchuPitchu <77695211+MatchuPitchu@users.noreply.github.com>
2021-11-18 17:06:55 +0100

    finish testing basics

--- src/App.test.js
+++ src/App.test.js
@@ -12,16 +12,21 @@ import App from './App';
 // 2) anonymous fn containing testing code
 test('renders learn react link', () => {
   // Arrange
+  // renders the entire component tree
   render(<App />);
 
   // Act
   // ...nothing
 
   // Assert
-  // screen: simulated browser; identifie element by a text (-> Regex case insensitive with learn react)
+  // screen: simulated browser; identifie element by a text (-> Regex case insensitive with learn react);
+  // types of methods:
+  // - get fns throw error if element is not found,
+  // - query fns return null if element is not found (don't throw error),
+  // - find fns return a promise (-> for asyncronous tasks if element is eventually on the screen)
   const linkElement = screen.getByText(/learn react/i);
   // test succeeds if element is found and else fails
   expect(linkElement).toBeInTheDocument();
 });
 
-// notice: when starting script 'npm test', tests are executed and file changes are watches, so tests are always re-executed instantly
+// notice: when starting script 'npm test', tests are executed and file changes are watches, so tests are always re-executed immediately
